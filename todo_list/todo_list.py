7b385fd6598c948a4052ad2a117245f6c5316dce
Ioannis Karakerezis <ykaraker@gmail.com>
2017-08-03 17:42:08 +0300

    add feature "remove from TODO list"

--- todo_list/todo_list.py
+++ todo_list/todo_list.py
@@ -20,7 +20,17 @@ def show_list():
         print("{}. {}".format(index, todo))
         index += 1
 
-    print("-"*30)
+    print("-"*10)
+
+def remove_from_list():
+    show_list()
+    what_to_remove = input("What would you like to remove?\n> ")
+    try:
+        todo_list.remove(what_to_remove)
+    except ValueError :
+        pass
+    show_list()
+    
 
 def show_help():
     clear_screen()
@@ -33,6 +43,7 @@ def show_help():
 Enter 'DONE' to stop adding tasks.
 Enter 'HELP' for this help.
 Enter 'SHOW' to see your current list.
+Enter 'REMOVE' to delete a 'TODO' from your list.
 """)
     
   
@@ -56,7 +67,7 @@ def add_to_list(todo):
         
     show_list()
 
-clear_screen()
+
 show_help()
 
 while True:
@@ -70,13 +81,15 @@ while True:
     elif new_todo.upper() == "HELP":
         show_help()
         continue
+    elif new_todo.upper() == "REMOVE":
+        remove_from_list()
+        continue
+        
     else:
         add_to_list(new_todo)
 
 
   
 
-
-      
     
     
